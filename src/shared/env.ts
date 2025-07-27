41334831334baa59b3a75ae27016af7b09e84d60
Paulo Patto <paulopatto@paulopatto.com>
2025-07-26 23:13:31 -0300

    chore(redis): Configure Redis prefix for queue and storage
    Configs a Redis prefix to isolate data bewtween envs trhough
    `REDIS_PREFIX` env var.
    
    Changes include:
    - Added REDIS_PREFIX environment variable with a default value.
    - Updated queue.ts to use the REDIS_PREFIX for the BullMQ queue.
    - Updated redis.ts to use the REDIS_PREFIX when creating the Redis connection.
    - Updated .env-sample to include the REDIS_PREFIX variable.
    
    This change allows running multiple environments using the same Redis instance.
    ```

--- src/shared/env.ts
+++ src/shared/env.ts
@@ -1,9 +1,19 @@
 import { z } from "zod";
-import "dotenv/config";
+import dotenv from "dotenv";
+
+let envFile = ".env";
+
+if (process.env.NODE_ENV === "test") envFile = ".env.test";
+if (process.env.NODE_ENV === "ci") envFile = ".env.ci";
+
+dotenv.config({
+  path: envFile
+});
 
 const envSchema = z.object({
   PORT: z.string().default("8080"),
   REDIS_URL: z.string(),
+  REDIS_PREFIX: z.string().default(""),
   PAYMENT_PROCESSOR_URL_DEFAULT: z.string(),
   PAYMENT_PROCESSOR_URL_FALLBACK: z.string(),
   PAYMENT_PROCESSOR_RETRY_LIMIT_BEFORE_USE_FALLBACK: z
@@ -13,4 +23,3 @@ const envSchema = z.object({
 });
 
 export const env = envSchema.parse(process.env);
-
