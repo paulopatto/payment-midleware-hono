951028e08d0545ee682525fd809927e4ed7bf9f9
Paulo Patto <paulopatto@paulopatto.com>
2025-07-31 11:37:47 -0300

    fix(ci): Add step to build .env on ci (#7)

--- src/payments/payment.service.ts
+++ src/payments/payment.service.ts
@@ -25,13 +25,14 @@ export async function createPaymentHandler(c: any) {
 export async function getSummaryHandler(c: any) {
   const summary = {
     default: {
-      totalRequests: await redis.get("summary:default:count") || 0,
-      totalAmount: await redis.get("summary:default:amount") || 0,
+      totalRequests: (await redis.get("summary:default:count")) || 0,
+      totalAmount: (await redis.get("summary:default:amount")) || 0,
     },
     fallback: {
-      totalRequests: await redis.get("summary:fallback:count") || 0,
-      totalAmount: await redis.get("summary:fallback:amount") || 0,
+      totalRequests: (await redis.get("summary:fallback:count")) || 0,
+      totalAmount: (await redis.get("summary:fallback:amount")) || 0,
     },
   };
   return c.json(summary);
 }
+
